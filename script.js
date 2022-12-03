33c47d35c1f78a06f260d537166a32b23d4c0edc
Seidali_ <117403561+Seidali001@users.noreply.github.com>
2022-12-03 14:27:53 +0000

    Add files via upload
    commit-1

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const str = "this is string"
+console.log(str);
+
+const numb = 12345;
+console.log(numb);
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+const t_r_u_e = true;
+console.log(t_r_u_e);
+
+const f_a_l_s_e = false;
+console.log(f_a_l_s_e);
+
+const empty = null;
+console.log(empty);
+
+let ind;
+console.log(ind);
+
+let user = {  
+  name: "Adam",
+  age: 25
+};
+console.log(user);
+
+const symbolSome = Symbol('some');
+console.log(typeof symbolSome);
